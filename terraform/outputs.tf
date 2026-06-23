1b9dcb92ed99c8154f9f22bdf471e5935a969fde
Tal Veneziano <tal@neureality.ai>
2026-06-23 11:55:03 +0300

    Initial Terraform EC2 project

---
+++ terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "instance_id" {
+  value = aws_instance.devops_project_3.id
+}
+
+output "public_ip" {
+  value = aws_instance.devops_project_3.public_ip
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
+variable "aws_profile" {
+  default = "IT"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
