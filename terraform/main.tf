d35b9cf497c6f84feb39befea69f8e8b88d7eab2
Tal Veneziano <tal@neureality.ai>
2026-06-29 14:19:38 +0300

    removed ingress HTTP

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,14 +88,6 @@ resource "aws_security_group" "devops_sg" {
     cidr_blocks = ["213.8.160.118/32"]
   }
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     description = "Allow all outbound"
     from_port   = 0
